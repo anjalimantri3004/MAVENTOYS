3cf20c9a3b2421fc0a3358a6f2ab902816a4d046
Anjali Mantri <anjalimantrii30@gmail.com>
2025-07-18 13:07:46 +0000

    egd

--- models/bronze/stg_sales.sql
+++ models/bronze/stg_sales.sql
@@ -1,14 +1,14 @@
 --SELECT * FROM MAVENTOYS.PUBLIC.SALES
 SELECT
-  CAST("Sale_ID" AS INTEGER) AS sale_id,
-  TO_DATE("Date", 'YYYY-MM-DD') AS sale_date,
-  CAST("Store_ID" AS INTEGER) AS store_id,
-  CAST("Product_ID" AS INTEGER) AS product_id,
-  CAST("Units" AS INTEGER) AS units
+  CAST('Sale_ID' AS INTEGER) AS sale_id,
+  TO_DATE('Date', 'YYYY-MM-DD') AS sale_date,
+  CAST('Store_ID' AS INTEGER) AS store_id,
+  CAST('Product_ID' AS INTEGER) AS product_id,
+  CAST('Units' AS INTEGER) AS units
 FROM {{ ref('sales') }}
 WHERE
-  "Sale_ID" IS NOT NULL
-  AND "Date" IS NOT NULL
-  AND "Store_ID" IS NOT NULL
-  AND "Product_ID" IS NOT NULL
-  AND "Units" > 0
+  'Sale_ID' IS NOT NULL
+  AND 'Date' IS NOT NULL
+  AND 'Store_ID' IS NOT NULL
+  AND 'Product_ID' IS NOT NULL
+  AND 'Units' > 0
