39a0889a65a0687207df97f07f89463c8e5fa088
Anjali Mantri <anjalimantrii30@gmail.com>
2025-07-20 06:47:19 +0000

    a

--- models/silver/int_calendar.sql
+++ models/silver/int_calendar.sql
@@ -1,22 +1,19 @@
 {{ config(materialized='table', schema='public') }}
 
 with source as (
-    select * from {{ ref('stg_calender') }}  -- assuming your bronze model is 'stg_calendar'
+    select * from {{ ref('stg_calendar') }}  -- assuming your bronze model is named 'stg_calendar'
 ),
 
 cleaned as (
     select
-        try_cast(calendar_date as date) as calendar_date,
-        cast(day as integer) as day,
-        cast(month as integer) as month,
-        cast(year as integer) as year,
-        cast(day_of_week as varchar) as weekday,
-        case
-            when lower(is_weekend) in ('yes', 'y', 'true', '1') then true
-            else false
-        end as is_weekend
+        try_cast("calendar_date" as date) as calendar_date,
+        try_cast("day" as integer) as day,
+        try_cast("month" as integer) as month,
+        try_cast("year" as integer) as year,
+        cast("day_of_week" as varchar) as day_of_week,
+        current_timestamp() as last_updated
     from source
-    where date is not null
+    where "date" is not null
 )
 
 select * from cleaned
