6786585e8860379a8a5a9ab7abb077432faf8956
Anjali Mantri <anjalimantrii30@gmail.com>
2025-07-20 04:23:56 +0000

    A

--- models/bronze/stg_calender.sql
+++ models/bronze/stg_calender.sql
@@ -1,1 +1,21 @@
-SELECT * FROM MAVENTOYS.PUBLIC.CALENDAR 
+{{ config(
+    materialized='table',
+    schema='public'
+) }}
+
+with source_data as (
+
+    select
+        try_cast("DATE" as date) as calendar_date,
+        date_part('day', try_cast("DATE" as date)) as day,
+        date_part('month', try_cast("DATE" as date)) as month,
+        date_part('year', try_cast("DATE" as date)) as year,
+        date_part('dow', try_cast("DATE" as date)) as day_of_week,  -- 0=Sunday in Snowflake
+        --to_char(try_cast("Date" as date), 'Day') as day_name,
+        --current_timestamp() as ingestion_timestamp
+
+    from {{ source('public', 'calendar') }}
+
+)
+
+select * from source_data
--- models/bronze/stg_dictionary.sql
+++ models/bronze/stg_dictionary.sql
@@ -1,1 +1,18 @@
-SELECT * FROM MAVENTOYS.PUBLIC.DICTIONARY
+{{ config(
+    materialized='table',
+    schema='public'
+) }}
+
+with source_data as (
+
+    select
+        trim("C1") as table_name,
+        trim("C2") as field_name,
+        trim("C3") as field_description,
+        
+
+    from {{ source('public', 'dictionary') }}
+
+)
+
+select * from source_data
