dcdf3a9b34e304d5dd8cf5f0ef4e026003de74bb
Anjali Mantri <anjalimantrii30@gmail.com>
2025-07-20 04:36:40 +0000

    A

--- models/bronze/stg_inventory.sql
+++ models/bronze/stg_inventory.sql
@@ -6,9 +6,9 @@
 with source_data as (
 
     select
-        cast("Store_ID" as varchar) as store_id,
-        cast("Product_ID" as varchar) as product_id,
-        try_cast("Stock_On_Hand" as integer) as stock_on_hand,
+        cast('Store_ID' as varchar) as store_id,
+        cast('Product_ID' as varchar) as product_id,
+        try_cast('Stock_On_Hand' as integer) as stock_on_hand,
         current_timestamp() as ingestion_timestamp
 
     from {{ source('public', 'inventory') }}
