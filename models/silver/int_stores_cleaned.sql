30c802495097e0a558b68b17772f8618dfd0ee86
Anjali Mantri <anjalimantrii30@gmail.com>
2025-07-20 06:00:45 +0000

    A

--- models/silver/int_stores_cleaned.sql
+++ models/silver/int_stores_cleaned.sql
@@ -14,8 +14,8 @@ renamed as (
     select
         cast("STORE_ID" as varchar) as store_id,
         initcap(trim("STORE_NAME")) as store_name,
-        upper(trim("LOCATION")) as location,
-        cast(nullif("CAPACITY", '') as integer) as capacity,
+        upper(trim('LOCATION')) as location,
+        cast(nullif('CAPACITY', '') as integer) as capacity,
         current_timestamp() as updated_at
 
     from source
